cc626526f771522436d98c21eebaa8abf5544104
Arby652 <95210928+Arby652@users.noreply.github.com>
2022-02-24 05:13:37 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+TOKEN = '5167570012:AAFjLHTU5e941EaklSZAyA0OGoqEbXVDsqk'
+
+keys = {
+    'bitcoin': 'BTC',
+    'ethereum': 'ETH',
+    'solana': 'SOL',
+    'tether': 'USDT',
+    'fantom': 'FTM',
+    'dollar':  'USD'
+}
